1da61b676cd846d4d98b57d018ae5361e5f0d34b
usma11dia0 <usma11dia0@outlook.com>
2022-03-08 19:58:01 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,10 @@ class ApplicationController < ActionController::Base
     unless logged_in?
       redirect_to login_url
     end
-  end 
+  end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
+  
 end
