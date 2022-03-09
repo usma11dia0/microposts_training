ca2aa57759890cb698b382ab8753efb1f330c4cb
usma11dia0 <usma11dia0@outlook.com>
2022-03-09 10:03:47 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました'
       redirect_to root_url
     else
-      @microposts = current_user.microposts.order('created_at DESC').page(paramas[:page])
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(paramas[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました'
       render 'toppages/index'
     end
